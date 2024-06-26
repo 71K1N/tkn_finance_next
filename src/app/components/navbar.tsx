ebc16c9a6e511e937cea5449370492deaf938184
Luiz Carlos <d71k1n@live.com>
2024-06-26 17:36:02 -0300

    feat(subcategory):basic crud created

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function navbar() {
+    return(
+        <>
+            <nav>
+                <a href="/">Home</a> |
+                <a href="/category/">Category</a> |
+                <a href="/#/">Subcategory</a> |
+                <a href="/#/">Accounts</a>
+            </nav>            
+        </>
+    )
+    
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./core.css";
+import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        <div className="container">
+          {children}
+          </div></body>
     </html>
   );
 }
